7169cb76920962681886bb6c2ff230dd15eebe0c
Ayon Chakraborty <61752403+ayonchak89@users.noreply.github.com>
2020-04-02 17:08:44 -0400

    AYON-6 Production Sanity Test Case Updated

--- Acceptancetest/src/test/java/acceptancetest/acat.java
+++ Acceptancetest/src/test/java/acceptancetest/acat.java
@@ -12,8 +12,8 @@ public class acat {
 	public HtmlUnitDriver driver;
 	//public WebDriver driver;
 	public WebDriverWait wait;
-	public String URL = "http://3.21.169.217:8080/ProdWebapp";
-	public String URL1 = "http://3.21.169.217:8080/ProdWebapp/avncreatepage.jsp";
+	public String URL = "http://23.101.203.147:8080/ProdWebapp";
+	public String URL1 = "http://23.101.203.147:8080/ProdWebapp/avncreatepage.jsp";
 	//public String URL = "http://localhost:8080/AVNCommunication-1.0/avnlogin.jsp";
 	//public String URL1 = "http://localhost:8080/AVNCommunication-1.0/avncreatepage.jsp";
 	
@@ -37,7 +37,7 @@ public class acat {
 		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 	}
 
-	@Test(priority = 1)
+/*	@Test(priority = 1)
 	public void repairticket() throws Exception {
 		WebElement welcome = wait.until(ExpectedConditions
 				.visibilityOfElementLocated(By.xpath("//h2[text()='WELCOME TO YOUR ACCOUNT OVERVIEW']")));
@@ -88,6 +88,6 @@ public class acat {
 		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 		//driver.findElement(By.xpath("//h1[text()='JUPITER COMMUNICATION']")).isDisplayed();
 		driver.quit();
-	}
+	} */
 
 }
